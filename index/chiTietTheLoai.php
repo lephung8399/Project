a594e78232763607cd920546c5eadd8a94465023
Hungdeptrai <lephung8399@gmail.com>
2019-01-25 08:56:04 +0700

    Go to the Top BUTTON
    t chỉnh luôn 3 trang. đã chạy ngon.

--- index/chiTietTheLoai.php
+++ index/chiTietTheLoai.php
@@ -24,6 +24,24 @@ session_start();
 			    right: -152px;
 			    color: #999
 			}
+			#myBtn {
+			  display: none;
+			  position: fixed;
+			  bottom: 20px;
+			  right: 30px;
+			  z-index: 99;
+			  font-size: 18px;
+			  border: none;
+			  outline: none;
+			  background-color: red;
+			  color: white;
+			  cursor: pointer;
+			  padding: 15px;
+			  border-radius: 4px;
+			}
+
+			#myBtn:hover {
+			  background-color: #555;
 		</style>
 	</head>
 	<body>
@@ -204,5 +222,25 @@ session_start();
 				</div>
 			</div>
 		</div>
+		<!-- Go to the Top BUTTON -->
+		<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+			<script>
+			// When the user scrolls down 20px from the top of the document, show the button
+			window.onscroll = function() {scrollFunction()};
+
+			function scrollFunction() {
+			  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			    document.getElementById("myBtn").style.display = "block";
+			  } else {
+			    document.getElementById("myBtn").style.display = "none";
+			  }
+			}
+
+			// When the user clicks on the button, scroll to the top of the document
+			function topFunction() {
+			  document.body.scrollTop = 0;
+			  document.documentElement.scrollTop = 0;
+			}
+			</script>
 	</body>
 	</html>
--- index/chiTietTinTuc.php
+++ index/chiTietTinTuc.php
@@ -27,6 +27,24 @@ mysqli_query($con,$sql);
 	    right: -152px;
 	    color: #999
 	}
+	#myBtn {
+			  display: none;
+			  position: fixed;
+			  bottom: 20px;
+			  right: 30px;
+			  z-index: 99;
+			  font-size: 18px;
+			  border: none;
+			  outline: none;
+			  background-color: red;
+			  color: white;
+			  cursor: pointer;
+			  padding: 15px;
+			  border-radius: 4px;
+			}
+
+			#myBtn:hover {
+			  background-color: #555;
 </style>
 </head>
 
@@ -252,6 +270,25 @@ mysqli_query($con,$sql);
 				
 			</div>
 
-	
+			<!-- Go to the Top BUTTON -->
+		<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+			<script>
+			// When the user scrolls down 20px from the top of the document, show the button
+			window.onscroll = function() {scrollFunction()};
+
+			function scrollFunction() {
+			  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+			    document.getElementById("myBtn").style.display = "block";
+			  } else {
+			    document.getElementById("myBtn").style.display = "none";
+			  }
+			}
+
+			// When the user clicks on the button, scroll to the top of the document
+			function topFunction() {
+			  document.body.scrollTop = 0;
+			  document.documentElement.scrollTop = 0;
+			}
+			</script>
 </body>
 </html> 
